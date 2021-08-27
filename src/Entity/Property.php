c1f629173cdf6cdfbdb152b389718e98071e0e77
Tahiry <tahirymariano.htm@gmail.com>
2021-08-27 10:53:03 +0300

    feat: frontend

---
+++ src/Entity/Property.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ */
+class Property
+{
+    const HEAT=[
+        0 =>'Electrique',
+        1 =>'Gaz'
+    ];
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $floor;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code_postal;
+
+    /**
+     * @ORM\Column(type="boolean",options={"default":false})
+     */
+    private $sold=false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bedroom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $room;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $heat;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
+        
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return (new Slugify())->slugify($this->title);
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getFloor(): ?int
+    {
+        return $this->floor;
+    }
+
+    public function setFloor(int $floor): self
+    {
+        $this->floor = $floor;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\dateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\dateTime $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getBedroom(): ?int
+    {
+        return $this->bedroom;
+    }
+
+    public function setBedroom(int $bedroom): self
+    {
+        $this->bedroom = $bedroom;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getRoom(): ?int
+    {
+        return $this->room;
+    }
+
+    public function setRoom(int $room): self
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+
+    public function getHeat(): ?int
+    {
+        return $this->heat;
+    }
+
+    public function setHeat(int $heat): self
+    {
+        $this->heat = $heat;
+
+        return $this;
+    }
+
+    public function getHeatType(): string
+    {
+        return self::HEAT[$this->heat];
+    }
+}
